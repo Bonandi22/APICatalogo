b672ae422deb21573ce2203bac9d564b6cc2f8da
Bonandi22 <elkerbonandi@gmail.com>
2023-11-08 11:23:56 +0000

    Create models and EF

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+namespace APICatalogo.Context;
+
+public class AppDbContext: DbContext
+{ 
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+  public DbSet<Category>? Categorias { get; set; }
+  public DbSet<Product>? Produtos { get; set; }
+
+
+    
+}
---
+++ APICatalogo/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
+
+public class Category
+{
+    public Category()
+    {
+        Products = new Collection<Product>(); //initialize the collection
+    }
+
+    public int CategoryId { get; set; }
+    public String? Name { get; set; }
+    public string? ImageUrl { get; set; }
+    public ICollection<Product>? Products { get; set; }
+
+}
