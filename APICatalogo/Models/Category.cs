3a5b877529a81f0c74a0ead41dda5a5f4edc00af
Bonandi22 <elkerbonandi@gmail.com>
2023-11-08 12:09:40 +0000

    data in table

--- APICatalogo/Models/Category.cs
+++ APICatalogo/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Models;
 
@@ -9,8 +10,15 @@ public class Category
         Products = new Collection<Product>(); //initialize the collection
     }
 
+    [Key]
     public int CategoryId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public String? Name { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public ICollection<Product>? Products { get; set; }
 
--- APICatalogo/Models/Product.cs
+++ APICatalogo/Models/Product.cs
@@ -1,12 +1,30 @@
-﻿namespace APICatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models;
 
 public class Product
 {
+    [Key]
     public int ProductId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Description { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public Decimal Price { get; set;}
+
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
+
     public float Stock { get; set;}
     public DateTime RegisterDate { get; set; }
     public int CategoryId { get; set; }
